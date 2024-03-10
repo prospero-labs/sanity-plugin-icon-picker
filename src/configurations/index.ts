8c7a96fea364089b31a4eef754d583078f1f7561
Travis Frank <travis@travismfrank.com>
2024-03-10 23:00:10 +0100

    feat: Remove imports of provider configurations to provide smaller bundle

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -1,12 +1,5 @@
-import f7 from './f7';
-import fa from './fa';
-import fi from './fi';
-import hi from './hi';
-import mdi from './mdi';
-import sa from './sa';
-import si from './si';
 import type { ProviderConfiguration } from '../types';
 
-const providers: ProviderConfiguration[] = [f7, fa, fi, hi, mdi, sa, si];
+const providers: ProviderConfiguration[] = [];
 
 export default providers;
